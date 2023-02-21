91c60b370844ce7e0a5a04db5dd1167ea2c51cc9
eric97li <eric97li@gmail.com>
2023-02-20 21:18:56 -0800

    Update DB with Driver location and display it

--- src/screens/setDriverInfo/SetDriverInfoScreen.tsx
+++ src/screens/setDriverInfo/SetDriverInfoScreen.tsx
@@ -11,14 +11,18 @@ const SetDriverInfoScreen = () => {
   const navigation = useNavigation<RootNavigationProp>();
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [showLoading, setShowLoading] = useState(true);
   const { driverLocation, setDriverLocation } = useStore().commonStore;
+  const { username } = useStore().commonStore;
 
   useEffect(() => {
     (async () => {
 
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      while (status !== 'granted') {
+        setErrorMsg('Please allow Raimei Location Access in your device settings.');
+        alert("Please allow Raimei Location Access in your device settings.");
+        navigation.navigate("Home");
         return;
       }
 
@@ -27,11 +31,11 @@ const SetDriverInfoScreen = () => {
     })();
   }, []);
 
-  let text = 'Waiting..';
+  // let text = 'Loading...';
   if (errorMsg) {
-    text = errorMsg;
+    // text = errorMsg;
   } else if (location) {
-    text = JSON.stringify(location);
+    // text = JSON.stringify(location);
     let latitude = location.coords.latitude;
     let longitude = location.coords.longitude;
 
@@ -45,7 +49,42 @@ const SetDriverInfoScreen = () => {
     fetch(addressLink)
       .then((response) => response.json())
       .then(data => {
+        setShowLoading(false);
         setDriverLocation(data.results[0].formatted_address);
+
+
+        let axios = require('axios');
+        let dataDB = JSON.stringify({
+            "collection": "users",
+            "database": "RaimeiDB",
+            "dataSource": "Cluster0",
+            "filter": { "username": username },
+            "update": {
+              "$set": {
+                "driverLocation": driverLocation
+              }
+            }
+        });
+    
+        let config = {
+            method: 'post',
+            url: 'https://data.mongodb-api.com/app/data-mqybs/endpoint/data/v1/action/updateOne',
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Request-Headers': '*',
+              'api-key': 'OuzpXWsAyFyncl3mEd4e19fXdXIzni6qi7KlcBzsKclyLAycPefVCE3iJe3om1I4',
+            },
+            data: dataDB
+        };
+    
+        axios(config)
+            .then(function (response) {
+                // console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
       })
 
   }
@@ -59,7 +98,15 @@ const SetDriverInfoScreen = () => {
         <Icon name="menu" />
       </TouchableOpacity>
       <Text style={{color: "black", fontSize: 50, marginTop: 100,marginBottom: 40, alignSelf:"center"}}>Set Driver Info</Text>
-      <Text style={{alignSelf:"center"}}>Current Location: {driverLocation}</Text>
+      { showLoading &&
+        <Text style={{alignSelf:"center"}}>Loading...</Text>
+      }
+      { (location) != null &&
+        <Text style={{alignSelf:"center"}}>Current Location: {driverLocation}</Text>
+      }
+      { (location) == null &&
+        <Text style={{alignSelf:"center"}}>{errorMsg}</Text>
+      }
     </View>
   );
 };
