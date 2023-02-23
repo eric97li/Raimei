d926b70e29671e4f2296ccaa6c1220ebbaf0f273
eric97li <eric97li@gmail.com>
2023-02-22 23:23:26 -0800

    Initial UI input fields and values on Home Screen

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,16 +1,41 @@
-import React from "react";
-import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import GooglePlacesSearchInput from "../../components/search/GooglePlacesSearchInput";
 import HomeOptions from "./HomeOptions";
 import { useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { MapStackParamList, RootNavigationProp } from "src/types/navigation";
-import { Icon } from "react-native-elements";
+import { CheckBox, Icon } from "react-native-elements";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const HomeScreen = () => {
   const Stack = createStackNavigator<MapStackParamList>();
   const navigation = useNavigation<RootNavigationProp>();
+
+  const [isSelected, setSelection] = useState();
+
+  const [mydate, setDate] = useState(new Date());
+  const [displaymodeDate] = useState('date');
+  const [isDisplayDate, setShowDate] = useState(isSelected);
+  const changeSelectedDate = (event, selectedDate) => {
+  const currentDate = selectedDate || mydate;
+  setDate(currentDate);
+};
+
+const [mydateTime, setDateTime] = useState(new Date());
+const [displaymodeTime] = useState('time');
+const [isDisplayDateTime, setShowTime] = useState(isSelected);
+const changeSelectedDateTime = (event, selectedDateTime) => {
+  const currentDateTime = selectedDateTime || mydateTime;
+  setDateTime(currentDateTime);
+};
+
+const set_selection = () => {
+  setSelection(!isSelected);
+  setShowTime(!isSelected);
+  setShowDate(!isSelected);
+}
 
   return (
     <SafeAreaView style={styles.screen}>
@@ -25,12 +50,67 @@ const HomeScreen = () => {
           style={styles.image}
           source={require("../../../assets/images/sakura-flower.png")}
         />
+        <View style={{marginBottom: 10}}></View>
         <GooglePlacesSearchInput
           type="origin"
           placeholder="Depart from?"
           styles={inputStyles}
         />
+
+      <View style={{flexDirection: 'row', marginTop: 10}}>
+      <CheckBox
+          checked={isSelected}
+          checkedColor='purple'
+          onPress={() => set_selection()}
+          title = "Reserve"
+          containerStyle ={{backgroundColor: "pink"}}
+        />
+
+      <TouchableOpacity style={{ borderWidth : 1, height : 30, width: 30
+              , justifyContent : "center", alignItems: "center", borderRadius: 30 ,
+              backgroundColor: "#4f284b", alignSelf: "center", marginLeft:5
+              }}
+              onPress={()=>alert("Check off to change default Ride request to Reserve Ride. Ride requested will follow Reserve Ride Guidelines. For details click on Information")}
+              >
+                <Text style={{color: "white"}}> ? </Text>
+                </TouchableOpacity>
+      </View>
+      <View style={{flexDirection: 'row', marginBottom: 10, marginTop: 5}}>
+      {isDisplayDate && (
+         <Text style={{marginLeft: 5, marginTop: "2.5%"}}> Date: </Text>
+         )}
+      {isDisplayDate && (
+        <DateTimePicker
+            testID="dateTimePicker"
+            value={mydate}
+            mode={displaymodeDate}
+            is24Hour={true}
+            display="default"
+            onChange={changeSelectedDate}
+            />
+         )}
+      {isDisplayDate && (
+         <Text style={{marginLeft: 5, marginTop: "2.5%"}}> Time: </Text>
+         )}
+      {isDisplayDateTime && (
+        <DateTimePicker
+            value={mydateTime}
+            mode={displaymodeTime}
+            is24Hour={true}
+            display="default"
+            onChange={changeSelectedDateTime}
+        />
+      )}
+      </View>
         <HomeOptions />
+        <TouchableOpacity style={{ borderWidth : 1, height : 42, width: "80%"
+              , justifyContent : "center", alignItems: "center", borderRadius: 40 ,
+              backgroundColor: "#4f284b", alignSelf: "center", marginTop: 20
+              }}
+              onPress={()=>alert("App Information")}
+              >
+                <Text style={{color: "white"}}> Information </Text>
+                </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
