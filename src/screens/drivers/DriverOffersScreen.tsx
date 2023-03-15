7a03833a53e1c84906660a668536a0c3691b6ccb
eric97li <eric97li@gmail.com>
2023-03-15 01:02:25 -0700

    Ride and Reserve Switch Component

--- src/screens/drivers/DriverOffersScreen.tsx
+++ src/screens/drivers/DriverOffersScreen.tsx
@@ -1,13 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Icon, Text } from "react-native-elements";
 import { RootNavigationProp } from "../../types/navigation";
+import { Switch } from 'react-native-switch';
 
 const DriverOffersScreen = () => {
   const navigation = useNavigation<RootNavigationProp>();
+  const [toggleRideReserve, setToggleRideReserve] = useState(true);
 
+  const toggleSwitch = () => {
+    setToggleRideReserve(!toggleRideReserve);
+    };
   return (
     <View>
       <TouchableOpacity
@@ -16,6 +21,28 @@ const DriverOffersScreen = () => {
       >
         <Icon name="menu" />
       </TouchableOpacity>
+      <View style={{ marginTop: 100,marginBottom: 40, alignSelf:"center"}}>
+      <Switch
+          value={toggleRideReserve}
+          onValueChange={()=>{
+          toggleSwitch();
+          }}
+          disabled={false}
+          activeText={'Ride'}
+          inActiveText={'Reserve'}
+          circleSize={40}
+          barHeight={40}
+          circleBorderWidth={3}
+          backgroundActive={"#4f284b"}
+          backgroundInactive={"#4f284b"}
+          circleActiveColor={"#BDB5D5"}
+          circleInActiveColor={"#BDB5D5"}
+          switchLeftPx={2}
+          switchRightPx={5}
+          switchWidthMultiplier={3}
+          switchBorderRadius={40}
+      />
+      </View>
       <Text style={{color: "black", fontSize: 50, marginTop: 100,marginBottom: 40, alignSelf:"center"}}>Driver Offers</Text>
     </View>
   );
